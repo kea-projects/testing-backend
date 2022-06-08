e7f6304c35e110d72a4d9f6eb3e024d8faed6d14
CristiPV <cristi.valentin29@gmail.com>
2022-06-08 08:53:05 +0200

    jest ignore docs folder

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  modulePathIgnorePatterns: ["build"],
+  modulePathIgnorePatterns: ["build", "docs"],
   testTimeout: 15000
 };
